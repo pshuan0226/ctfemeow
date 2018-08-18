6df7f7faf6b021274997d3445347f63cdd2036b8
Patricia Huang <siahuang@umich.edu>
2018-08-18 10:17:41 -0500

    Update ctfemeow.py

--- ctfemeow.py
+++ ctfemeow.py
@@ -2,8 +2,12 @@
 
 import smtplib
 import requests
+import email.MIMEText
+import email.MIMEBase
+from email.MIMEMultipart import MIMEMultipart
 from datetime import datetime
 from icalendar import Calendar, Events
+
 
 #check for date
 stime = datetime.today().strftime('%s')
@@ -18,15 +22,42 @@ ctfs = response.json()
 
 #create calendar file
 cal = Calendar()
+for ctf in ctfs:
+    #only add online ctfs
+    if ctf['onsite'] == 'true':
+        continue
+    event = Event()
+    event.add('summary', ctf['title'])
+    event.add('dstart', ctf['start'])
+    event.add('dend', ctf['finish'])
+    event.add('description', ctf['description'])
+    cal.add_component(event)
+
+filename = 'ctf_events_' + today.year + '_' + today.month
+f = open('%s.ics' % filename, 'wb')
+f.write(cal.to_ical())
+f.close()
 
 #grab email credentials for sender
 config = {}
 execfile('config.py', config)
 
+#craft message
+msg = MIMEMultipart('alternative')
+msg['Subject'] = 'CTFs of %s-%s' % (today.year, today.month)
+msg['From'] = config['sender_email_id'] + '@gmail.com'
+msg['To'] = config['sender_email_id'] + '@gmail.com'
+
+#attach .ics file
+icsfile = email.MIMEBase.MIMEBase('text', "calendar", method="REQUEST", name=filename)
+email.Encoders.encode_base64(icsfile)
+icsfile.add_header('Content-Description', filename)
+icsfile.add_header('Content-class', 'urn:content-classes:calendarmessage')
+msg.attach(icsfile)
+
 #create session to send email
 session = smtplib.SMTP('smtp.gmail.com', 587)
 session.starttls()
 session.login(config['sender_email_id'], config['sender_email_id_password'])
-message = 'test'
-session.sendmail(config['sender_email_id'], config['sender_email_id_password'], message)
+session.sendmail(config['sender_email_id'], config['sender_email_id_password'], msg)
 session.quit()
